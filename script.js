48f481f7cbaebf451431406d335f3425f18a68db
David Cabezas <dxabezas@gmail.com>
2020-08-27 17:58:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const generator = tf.loadLayersModel('./TFJS_GAN-generator/model.json',(fraction: number) => void);
+(async () => {
+  const generator = tf.loadLayersModel('./TFJS_GAN-generator/model.json');
+})
 
 console.log('Hello ImgLux');
