1e0c859cbfaf25b00747a5d6c72f00ee3a229d25
David Cabezas Berrido <dxabezas@gmail.com>
2020-08-28 10:29:38 +0200

    No permite subir imágenes hasta que el modelo esté cargado

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 
 var generator;
+var ready = false;
 
 function progress(fraction){
-    document.getElementById("prog").innerHTML = parseInt(fraction*100);
+    document.getElementById("prog").innerHTML = 'Loading model: '+parseInt(fraction*100).toString()+' %';
 }
 
 async function init(){
+    document.getElementById("subm").style.visibility="hidden";
+    
     generator = await tf.loadLayersModel('./TFJS_GAN-generator/model.json', {strict : false, onProgress : progress});
 }
 
+function run(inimg){
+    console.log(typeof(inimg));
+}
+
 init().then(() => {
-    document.getElementById("prog").innerHTML = 'READY!';
-    console.log(generator.summary());
+    document.getElementById("prog").innerHTML = 'READY! :D\nThe model has been loaded, you can now submit a dark photo to light it up.';
+    //console.log(generator.summary());
+
+    ready = true;
+    document.getElementById("subm").style.visibility="visible";
 });
 
 
