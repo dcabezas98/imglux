18c92eb1d5862dc9dfc92a48352aa4b431cddd47
David Cabezas Berrido <dxabezas@gmail.com>
2020-08-28 09:04:15 +0200

    Carga generator con progreso

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-/*(async () => {
-  const generator = tf.loadLayersModel('./TFJS_GAN-generator/model.json');
-})
-*/
 
-console.log('Hello ImgLux');
+function progress(fraction){
+    document.getElementById("prog").innerHTML = fraction;
+}
+
+async function init(){
+    const generator = await tf.loadLayersModel('./TFJS_GAN-generator/model.json', {strict : false, onProgress : progress});
+}
+
+init();
+console.log(generator.summary());
